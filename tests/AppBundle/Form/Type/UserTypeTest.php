5d37375dc8b974df7bc9685ecf403e99933ed395
opportus <opportus@gmail.com>
2019-07-23 02:08:32 +0200

    Removes password field check from the user type test

--- tests/AppBundle/Form/Type/UserTypeTest.php
+++ tests/AppBundle/Form/Type/UserTypeTest.php
@@ -4,10 +4,13 @@ namespace Tests\AppBundle\Form\Type;
 
 use AppBundle\Entity\User;
 use AppBundle\Form\Type\UserType;
+use Symfony\Component\Form\Test\Traits\ValidatorExtensionTrait;
 use Symfony\Component\Form\Test\TypeTestCase;
 
 class UserTypeTest extends TypeTestCase
 {
+    use ValidatorExtensionTrait;
+
     public function testSubmitValidData()
     {
         $userToCompare = new User();
@@ -16,13 +19,11 @@ class UserTypeTest extends TypeTestCase
 
         $user = new User();
         $user->setUsername('Meli Melo');
-        $user->setPassword('azerty');
         $user->setEmail('melimelo@example.com');
         $user->setRole(User::ROLE_USER);
 
         $formData = [
             'username' => 'Meli Melo',
-            'password' => 'azerty',
             'email'    => 'melimelo@example.com',
             'role'     => User::ROLE_USER
         ];
